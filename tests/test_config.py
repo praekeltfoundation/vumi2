ec01c336710fd64b802e652998894908a40859b3
Jeremy Thurgood <jeremy@praekelt.org>
2023-04-18 11:19:50 +0200

    Add flake8-bandit lints and fix violations

--- tests/test_config.py
+++ tests/test_config.py
@@ -15,8 +15,8 @@ def test_default_base_config():
     config = BaseConfig.deserialise({})
     assert config.amqp.hostname == "127.0.0.1"
     assert config.amqp.port == 5672
-    assert config.amqp.username == "guest"
-    assert config.amqp.password == "guest"
+    assert config.amqp.username == "guest"  # noqa: S105 (These are fake creds.)
+    assert config.amqp.password == "guest"  # noqa: S105 (These are fake creds.)
     assert config.amqp.vhost == "/"
     assert config.amqp_url == ""
     assert config.worker_concurrency == 20
@@ -38,8 +38,8 @@ def test_specified_base_config():
     )
     assert config.amqp.hostname == "localhost"
     assert config.amqp.port == 1234
-    assert config.amqp.username == "user"
-    assert config.amqp.password == "pass"
+    assert config.amqp.username == "user"  # noqa: S105 (These are fake creds.)
+    assert config.amqp.password == "pass"  # noqa: S105 (These are fake creds.)
     assert config.amqp.vhost == "/vumi"
     assert config.amqp_url == "amqp://user:pass@localhost:1234/vumi"
     assert config.worker_concurrency == 10
