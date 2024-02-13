e803ba7ee743d3a973825c1ddfbbc113e7b7d866
Erik Harding <erik@praekelt.org>
2024-02-13 11:10:42 +0200

    fix black

--- tests/test_workers.py
+++ tests/test_workers.py
@@ -80,7 +80,7 @@ class SlowSetupWorker(BaseWorker):
     """
 
     async def setup(self):
-        self.s_exc, self.exc = open_memory_channel[Optional[Exception]|None](1)
+        self.s_exc, self.exc = open_memory_channel[Optional[Exception] | None](1)
         await self.setup_receive_inbound_connector("ri", self.handle_in, self.handle_ev)
         await sleep(0.1)
         await self.setup_receive_outbound_connector("ro", self.handle_out)
