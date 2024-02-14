bf27b6ded6400dd6cf5ed127545f0a3fe7e9eacd
Erik Harding <erik@praekelt.org>
2024-02-14 14:16:56 +0200

    Add static reply application

---
+++ src/vumi2/applications/static_reply/static_reply.py
@@ -0,0 +1,46 @@
+from attrs import define
+
+from vumi2.messages import Event, Message, Session
+from vumi2.workers import BaseConfig, BaseWorker
+
+
+@define(kw_only=True)
+class StaticReplyConfig(BaseConfig):
+    transport_name: str
+    reply_text: str
+
+
+class StaticReplyApplication(BaseWorker):
+    """
+    A static reply application
+
+    It will reply to any inbound message with the exonfigured reply text
+    """
+
+    config: StaticReplyConfig
+
+    async def setup(self) -> None:
+        await self.setup_receive_outbound_connector(
+            connector_name=self.config.transport_name,
+            outbound_handler=self.handle_outbound_message,
+        )
+        await self.setup_receive_inbound_connector(
+            connector_name=self.config.transport_name,
+            inbound_handler=self.handle_inbound_message,
+            event_handler=self.handle_event,
+        )
+
+        await self.start_consuming()
+
+    async def handle_inbound_message(self, message: Message):
+        reply_msg = message.reply(self.config.reply_text, Session.CLOSE)
+
+        await self.receive_outbound_connectors[
+            self.config.transport_name
+        ].publish_inbound(reply_msg)
+
+    async def handle_event(self, event: Event):
+        pass
+
+    async def handle_outbound_message(self, message: Message):
+        pass
