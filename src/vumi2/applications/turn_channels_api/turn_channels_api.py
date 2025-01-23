9d4fd6d991a956b5b414fb3602d357e41b9c6a67
Gerrit Vermeulen <gerrit@reachdigitalhealth.org>
2025-01-23 15:02:23 +0200

    format file

--- src/vumi2/applications/turn_channels_api/turn_channels_api.py
+++ src/vumi2/applications/turn_channels_api/turn_channels_api.py
@@ -194,7 +194,8 @@ class TurnChannelsApi(BaseWorker):
                         computed_signature = str(base64.b64encode(h))
                         signature = request.headers.get("X-Turn-Hook-Signature", "")
                         logger.info(
-                            f"Signature from Turn: {signature}. Computed: {computed_signature}"
+                            f"Signature from Turn: {signature}."
+                            "Computed: {computed_signature}"
                         )
                         if not hmac.compare_digest(computed_signature, signature):
                             raise SignatureMismatchError()
