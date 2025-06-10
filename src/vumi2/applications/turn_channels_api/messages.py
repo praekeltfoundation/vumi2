cd251eb0e5623a105a7dc38757d6ea8593933e59
Gerrit Vermeulen <gerrit@reachdigitalhealth.org>
2025-06-10 12:06:22 +0200

    Add documentation for Turn Channels

--- src/vumi2/applications/turn_channels_api/messages.py
+++ src/vumi2/applications/turn_channels_api/messages.py
@@ -84,7 +84,7 @@ def turn_event_from_ev(event: Event) -> dict:
     if event.event_type == EventType.DELIVERY_REPORT:
         ev["status"]["status"] = {
             DeliveryStatus.PENDING: "sent",
-            DeliveryStatus.FAILED: "sent",
+            DeliveryStatus.FAILED: "sent", # TODO: Turn doesn't accept a failed status. Log the failure for now?
             DeliveryStatus.DELIVERED: "delivered",
             None: None,
         }[event.delivery_status]
