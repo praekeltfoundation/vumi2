c562ec378b7f4c00dfb9a4cb90ab77668e345e6a
Matthew Weppennaar <matthewweppennaar@Matthews-MacBook-Air-2.local>
2025-06-18 14:23:26 +0200

    reformatiing

--- src/vumi2/middlewares/logging.py
+++ src/vumi2/middlewares/logging.py
@@ -1,13 +1,14 @@
 from attr import define
-from vumi2.middlewares.base import  BaseMiddleware, BaseMiddlewareConfig
+from vumi2.middlewares.base import BaseMiddleware, BaseMiddlewareConfig
 from logging import getLogger, getLevelNamesMapping
+
 
 @define
 class LoggingMiddlewareConfig(BaseMiddlewareConfig):
-    #TODO: vallidate log level
+    # TODO: vallidate log level
     log_level: str = "info"
     logger_name: str = __name__
-    
+
 
 class LoggingMiddleware(BaseMiddleware):
     config: LoggingMiddlewareConfig
@@ -15,13 +16,12 @@ class LoggingMiddleware(BaseMiddleware):
     async def setup(self):
         self.log_level = getLevelNamesMapping()[self.config.log_level.upper()]
         self.logger = getLogger(self.config.logger_name)
-        
+
     def _log_msg(self, direction, msg, connector_name):
-        self.logger.log(self.log_level,
-            f"Processed {direction} message for {connector_name}: {msg}"
+        self.logger.log(
+            self.log_level, f"Processed {direction} message for {connector_name}: {msg}"
         )
         return msg
-
 
     async def handle_inbound(self, message, connector_name):
         return self._log_msg("inbound", message, connector_name)
@@ -31,4 +31,3 @@ class LoggingMiddleware(BaseMiddleware):
 
     async def handle_event(self, event, connector_name):
         return self._log_msg("event", event, connector_name)
-
--- tests/middlewares/test_logging.py
+++ tests/middlewares/test_logging.py
@@ -1,6 +1,7 @@
 from vumi2.middlewares.logging import LoggingMiddleware, LoggingMiddlewareConfig
 from vumi2.messages import Event, EventType, Message, TransportType
 
+
 def mkmsg(content: str) -> Message:
     return Message(
         to_addr="12345",
@@ -17,9 +18,11 @@ def mkev(msg_id: str) -> Event:
         event_type=EventType.ACK,
         sent_message_id=msg_id,
     )
-#Add doc string
+
+
+# Add doc string
 async def test_handle_inbound():
-    #TODO: make sure we actually log the thing assert that message is logged
+    # TODO: make sure we actually log the thing assert that message is logged
     config = LoggingMiddlewareConfig(
         "vumi2.middlewares.logging.LoggingMiddleware",
         enable_for_connectors=["connection2"],
@@ -28,4 +31,4 @@ async def test_handle_inbound():
     middleware = LoggingMiddleware(config)
     await middleware.setup()
     message = mkmsg("Hello")
-    assert await middleware.handle_inbound(message,"connection1") == message
+    assert await middleware.handle_inbound(message, "connection1") == message
